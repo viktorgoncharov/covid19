a6d648d65d6f58046cb7a8bd7838aa98709e42cb
viktorgoncharov <viktor.y.goncharov@gmail.com>
2020-05-06 19:55:00 +0300

    The first implementation

--- src/main/java/com/virhon/tests/covid19/api/COVID19Client.java
+++ src/main/java/com/virhon/tests/covid19/api/COVID19Client.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Optional;
 
--- src/main/java/com/virhon/tests/covid19/domain/CountryCase.java
+++ src/main/java/com/virhon/tests/covid19/domain/CountryCase.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
-import java.time.ZonedDateTime;
 import java.util.Date;
 
 @Entity(name = "CountryCase")
